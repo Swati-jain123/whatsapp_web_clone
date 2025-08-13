de854bd4a67df0fd3be7a94001cae5dbf90cb504
Swati Jain <98042616+Swati-jain123@users.noreply.github.com>
2025-08-13 12:44:57 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -106,7 +106,7 @@ app.post("/messages", async (req, res) => {
 
 // ✅ Serve React frontend after build (FIXED for Render)
 app.use(express.static(path.join(__dirname, "../frontend/build")));
-app.get("*", (req, res) => {
+app.get("/*splat", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
 });
 
